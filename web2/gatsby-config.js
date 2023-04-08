57615cfd7fd06d6729f168d1e0a68768da0be58c
Alexander Dollard <alexdollard141@gmail.com>
2023-04-07 19:16:27 -0500

    gatsby added by being sneaky

---
+++ web2/gatsby-config.js
@@ -0,0 +1,23 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `cyan-snake-gatsby`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-sanity',
+    options: {
+      "projectId": "86hsgap4",
+      "dataset": ""
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
