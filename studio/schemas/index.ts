99dc1985c90680ca0f71b0713efc39df50ccaec2
Alexander Dollard <alexdollard141@gmail.com>
2023-04-12 08:18:23 -0500

    schemas upload

--- studio/schemas/index.ts
+++ studio/schemas/index.ts
@@ -1,5 +1,22 @@
-import pet from './pet'
+import author from './documents/author';
+import blog from './documents/blog';
+import category from './documents/category';
+import featured from './documents/featured';
+import customCode from './objects/customCode';
+import customImage from './objects/customImage';
+import normalText from './objects/normalText';
+import richText from './objects/richText';
 
 export const schemaTypes = [
-  pet
+  //documents
+  author,
+  blog,
+  category,
+  featured,
+
+  //objects
+  customCode,
+  customImage,
+  normalText,
+  richText,
 ]
